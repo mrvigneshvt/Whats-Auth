6070ac81fb9558147060a6079d3c44cca0780a11
mrvigneshvt <mrvigneshvt@gmail.com>
2025-02-14 14:59:39 +0530

    added pinging to root

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "root-app",
-      script: "./root.js",
+      script: "./src/root.js",
       instances: 1,
       autorestart: true,
       watch: false,
